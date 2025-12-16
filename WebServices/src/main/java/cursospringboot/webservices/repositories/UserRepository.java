523f7a8d8c7454a411d585bd48c6515393ee1813
Leonardo <leonardo.capristo@hotmail.com>
2025-12-16 16:55:00 -0300

    att

--- WebServices/src/main/java/cursospringboot/webservices/repositories/UserRepository.java
+++ WebServices/src/main/java/cursospringboot/webservices/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package cursospringboot.webservices.repositories;
 
 import cursospringboot.webservices.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
---
+++ WebServices/src/main/java/cursospringboot/webservices/services/UserService.java
@@ -0,0 +1,30 @@
+package cursospringboot.webservices.services;
+
+import cursospringboot.webservices.entities.User;
+import cursospringboot.webservices.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+
+        Optional<User> obj = userRepository.findById(id);
+
+        return obj.get();
+    }
+}
