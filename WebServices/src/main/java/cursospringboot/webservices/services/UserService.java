ec11394dcb443fa29cd3ae7399a21513a463d9f3
Leonardo <leonardo.capristo@hotmail.com>
2025-12-16 18:15:37 -0300

    att

--- WebServices/src/main/java/cursospringboot/webservices/services/UserService.java
+++ WebServices/src/main/java/cursospringboot/webservices/services/UserService.java
@@ -15,16 +15,14 @@ public class UserService {
     UserRepository userRepository;
 
 
-
-
     public List<User> findAll(){
         return userRepository.findAll();
     }
 
     public User findById(Long id){
-
         Optional<User> obj = userRepository.findById(id);
-
         return obj.get();
     }
+
+
 }
