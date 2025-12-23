661c9049e6ea559703914a40114f6bdfcd9eed26
Leonardo <leonardo.capristo@hotmail.com>
2025-12-23 14:02:44 -0300

    database seeding

--- dscatalog/src/main/java/CursoSpringBoot/dscatalog/controllers/CategoryController.java
+++ dscatalog/src/main/java/CursoSpringBoot/dscatalog/controllers/CategoryController.java
@@ -1,6 +1,7 @@
 package CursoSpringBoot.dscatalog.controllers;
 
 import CursoSpringBoot.dscatalog.entities.Category;
+import CursoSpringBoot.dscatalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +13,16 @@ import java.util.List;
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
 
-        List<Category> list = List.of(
-                new Category(1L, "Books"),
-                new Category(2L, "Electronics"),
-                new Category(3L, "Computers"),
-                new Category(4L, "Home Appliances")
-        );
+        List<Category> list = categoryService.findAll();
 
         return ResponseEntity.ok().body(list);
     }
---
+++ dscatalog/src/main/java/CursoSpringBoot/dscatalog/services/CategoryService.java
@@ -0,0 +1,26 @@
+package CursoSpringBoot.dscatalog.services;
+
+import CursoSpringBoot.dscatalog.entities.Category;
+import CursoSpringBoot.dscatalog.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+
+
+
+}
