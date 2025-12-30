e1513216d68ff5d70a162f8700da28f9dfb23b98
Leonardo <leonardo.capristo@hotmail.com>
2025-12-30 15:58:46 -0300

    crud e camadas de product

---
+++ dscatalog/src/main/java/CursoSpringBoot/dscatalog/controllers/ProductController.java
@@ -0,0 +1,73 @@
+package CursoSpringBoot.dscatalog.controllers;
+
+import CursoSpringBoot.dscatalog.dto.ProductDTO;
+import CursoSpringBoot.dscatalog.services.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAllPaged(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Page<ProductDTO> list = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<ProductDTO>> findAll(){
+        List<ProductDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+
+        dto = service.update(id,dto);
+        return ResponseEntity.ok().body(dto);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> delete(@PathVariable Long id){
+
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+}
---
+++ dscatalog/src/main/java/CursoSpringBoot/dscatalog/services/ProductService.java
@@ -0,0 +1,92 @@
+package CursoSpringBoot.dscatalog.services;
+
+import CursoSpringBoot.dscatalog.dto.ProductDTO;
+import CursoSpringBoot.dscatalog.entities.Product;
+import CursoSpringBoot.dscatalog.exceptions.DatabaseException;
+import CursoSpringBoot.dscatalog.exceptions.ResourceNotFoundException;
+import CursoSpringBoot.dscatalog.repositories.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Product> list = repository.findAll(pageRequest);
+        return list.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductDTO> findAll() {
+        List<Product> list = repository.findAll();
+        List<ProductDTO> listDto = list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+
+        /*
+        List<ProductDTO> listDto = new ArrayList<>();
+        for (Product category : list) {
+            listDto.add(new ProductDTO(category));
+        }
+         */
+        return listDto;
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
+        return new ProductDTO(entity, entity.getCategories());
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        //entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+    try {
+        Product entity = repository.getReferenceById(id);
+        //entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+
+    }catch (EntityNotFoundException e){
+        throw new ResourceNotFoundException("Id não encontrado: " + id);
+    }
+
+    }
+
+
+    public void delete(Long id) {
+        try{
+            repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id não encontrado: " + id));
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id não encontrado: " + id);
+
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Integridade de dados");
+        }
+
+    }
+
+
+}
