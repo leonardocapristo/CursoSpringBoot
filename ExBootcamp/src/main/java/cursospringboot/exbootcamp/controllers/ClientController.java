6e6058bc610bba154bd05a7da8a5e3987f56b598
Leonardo <leonardo.capristo@hotmail.com>
2025-12-30 17:33:09 -0300

    find by id

--- ExBootcamp/src/main/java/cursospringboot/exbootcamp/controllers/ClientController.java
+++ ExBootcamp/src/main/java/cursospringboot/exbootcamp/controllers/ClientController.java
@@ -6,10 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -32,5 +29,11 @@ public class ClientController {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         Page<ClientDTO> list = service.findAll(pageRequest);
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- ExBootcamp/src/main/java/cursospringboot/exbootcamp/services/ClientServices.java
+++ ExBootcamp/src/main/java/cursospringboot/exbootcamp/services/ClientServices.java
@@ -7,18 +7,25 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ClientServices {
 
-    ClientRepository clientRepository;
+    ClientRepository repository;
 
-    public ClientServices(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
+    public ClientServices(ClientRepository repository) {
+        this.repository = repository;
     }
 
     public Page<ClientDTO> findAll(PageRequest pageRequest){
-        Page<Client> result = clientRepository.findAll(pageRequest);
+        Page<Client> result = repository.findAll(pageRequest);
         return result.map(x -> new ClientDTO(x));
+    }
+
+    public ClientDTO findById(Long id) {
+        Client entity = repository.findById(id).get();
+        return new ClientDTO(entity);
 
     }
 
