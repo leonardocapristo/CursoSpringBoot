36389c53febc603f1ffb589c1de18e56dacf15c8
Leonardo <leonardo.capristo@hotmail.com>
2026-01-06 15:09:00 -0300

    Fixtures no Junit, beforeach

--- dscatalog/src/test/java/CursoSpringBoot/dscatalog/repositories/ProductRepositoryTests.java
+++ dscatalog/src/test/java/CursoSpringBoot/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,11 +1,14 @@
 package CursoSpringBoot.dscatalog.repositories;
 
 import CursoSpringBoot.dscatalog.entities.Product;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
+
 
 import java.util.Optional;
 
@@ -15,19 +18,34 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+    private long nonExistingId = 1000L;
+    private long existingId = 1L;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        nonExistingId = 1000L;
+        existingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
-        repository.deleteById(1L);
-        Optional<Product> result =  repository.findById(1L);
+        repository.deleteById(existingId);
+        Optional<Product> result =  repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
 
     @Test
     public void deleteShouldDoNothingWhenIdDoesNotExists(){
-        long nonExistingId = 1000L;
+
+        // Verifica que não existe
         Assertions.assertFalse(repository.existsById(nonExistingId));
+
+        // Não deve lançar exceção
         Assertions.assertDoesNotThrow(() -> {
             repository.deleteById(nonExistingId);
         });
     }
+
+
 }
