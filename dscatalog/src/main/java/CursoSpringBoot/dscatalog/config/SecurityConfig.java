cb611cbe8599e2e2094ee01cba8e3489d836c7c6
Leonardo <leonardo.capristo@hotmail.com>
2026-01-13 09:47:43 -0300

    update banco h2

--- dscatalog/src/main/java/CursoSpringBoot/dscatalog/config/SecurityConfig.java
+++ dscatalog/src/main/java/CursoSpringBoot/dscatalog/config/SecurityConfig.java
@@ -11,8 +11,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+
         return http.build();
     }
 }
