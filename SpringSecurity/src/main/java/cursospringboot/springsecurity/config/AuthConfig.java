90229a0213def6e32cfa5626c390001d8d7c2c65
Leonardo Capristo <leonardo.capristo@hotmail.com>
2026-02-02 17:59:35 -0300

    min 43

---
+++ SpringSecurity/src/main/java/cursospringboot/springsecurity/config/AuthConfig.java
@@ -0,0 +1,4 @@
+package cursospringboot.springsecurity.config;
+
+public class AuthConfig {
+}
--- SpringSecurity/src/main/java/cursospringboot/springsecurity/controller/AuthController.java
+++ SpringSecurity/src/main/java/cursospringboot/springsecurity/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ public class AuthController {
     public ResponseEntity<LoginResponse> login (@Valid @RequestBody LoginRequest loginRequest){
 
         UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(loginRequest.email(),loginRequest.password());
+        Authentication auth = authenticationManager.authenticate(userAndPass);
 
         return null;
     }
